c30490a9d406485aa0906a98735108a128701d87
Maximilien Schall-Fonteilles <maximilien.schall@getakeneo.com>
2025-04-01 16:55:56 +0200

    Deployment tests

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -18,8 +18,10 @@ export async function decrypt(session: string | undefined = "") {
     const { payload } = await jwtVerify(session, encodedKey, {
       algorithms: ["HS256"],
     });
+
     return payload;
   } catch (error) {
+    console.error(error);
     console.log("Failed to verify session");
   }
 }
